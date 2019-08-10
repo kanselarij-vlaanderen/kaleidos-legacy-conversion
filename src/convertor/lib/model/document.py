4d1829924e69a931c3f32b511dbad27e71f0e537
Michaël Dierick <michael.dierick@redpencil.io>
2019-08-10 19:11:11 +0200

    Set double property for document stuknummer

--- src/convertor/lib/model/document.py
+++ src/convertor/lib/model/document.py
@@ -73,6 +73,7 @@ class Document:
                             URIRef(self.access_level_uri)))
         if self.name:
             triples.append((uri, ns.BESLUITVORMING['stuknummerVR'], Literal(self.name)))
+            triples.append((uri, ns.EXT['stuknummerVROriginal'], Literal(self.name)))
         # if self.title:
         #     triples.append((uri, ns.DCT['title'], Literal(self.title)))
         # if self.description:
